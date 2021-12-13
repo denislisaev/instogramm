cb3521c3ee330b25c7609f13cfb916b2fbecba69
Denis <denislisaev@yandex.ru>
2021-12-14 01:46:40 +0300

    finish

--- src/main/java/com/instogramm/instogramm/sucurity/WebSecurityConfig.java
+++ src/main/java/com/instogramm/instogramm/sucurity/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManager() throws Exception{
-        return super.authenticationManagerBean();
+        return super.authenticationManager();
     }
 
     @Bean
